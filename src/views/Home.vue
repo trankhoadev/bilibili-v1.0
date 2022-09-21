6e8189eace9e6cb48a6bd591f9f6f4d0d1b1364c
trankhoadev <trankhoadev@gmail.com>
2022-09-21 17:07:47 +0700

    Change project structure for get more productivity

---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template lang="">
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad nisi, mollitia laborum itaque sequi nam voluptatibus. Itaque quas fugit tempora vitae incidunt. Saepe reiciendis nostrum dolor veritatis quos quis dolores?
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="">
+    
+</style>
